3c1b55b7a693cfee0e561e56e9d036608ea080a8
Kabirsharma2607 <kabirsharma2607@gmail.com>
2024-07-15 01:39:29 +0530

    Added Server Sidebar

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -24,13 +24,13 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
-          className="bg-transparent"
+          className="rounded-[24px]"
           variant="outline"
           size="icon"
           onClick={onClick}
         >
-          <Sun className="text-black h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 border-black solid" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 border-0" />
+          <Sun className="text-black h-[1.2rem] w-[1.2rem] rotate-0 scale-100  transition-all dark:-rotate-90 dark:scale-0 border-black solid" />
+          <Moon className="bg-transparent absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 border-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>

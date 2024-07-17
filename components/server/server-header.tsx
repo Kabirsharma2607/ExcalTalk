018b3754f7010e7e692496fbdfff3d3a17f6ec9e
Kabirsharma2607 <kabirsharma2607@gmail.com>
2024-07-17 14:19:39 +0530

    Added Delete Server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -79,7 +79,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem className="text-rose-500 hover:font-bold text-sm px-3 py-2 cursor-pointer">
+          <DropdownMenuItem
+            className="text-rose-500 hover:font-bold text-sm px-3 py-2 cursor-pointer"
+            onClick={() => onOpen("deleteServer", { server })}
+          >
             Delete Server
             <Trash2 className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
